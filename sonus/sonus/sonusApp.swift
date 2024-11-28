7bd8264934f0d0835c533a17aead5142148333b4
fonztara <fonztara@gmail.com>
2024-11-28 16:31:13 +0100

    Initial Commit

---
+++ sonus/sonus/sonusApp.swift
@@ -0,0 +1,17 @@
+//
+//  sonusApp.swift
+//  sonus
+//
+//  Created by Alfonso Tarallo on 28/11/24.
+//
+
+import SwiftUI
+
+@main
+struct sonusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sonus/sonusTests/sonusTests.swift
@@ -0,0 +1,17 @@
+//
+//  sonusTests.swift
+//  sonusTests
+//
+//  Created by Alfonso Tarallo on 28/11/24.
+//
+
+import Testing
+@testable import sonus
+
+struct sonusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
